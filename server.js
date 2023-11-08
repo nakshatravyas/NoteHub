333cde19ad787f7a1bf15198dac3af405cd868e2
Nakshatra Vyas <125440526+nakshatravyas@users.noreply.github.com>
2023-11-08 13:51:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const userRouter = require('./routes/userRouter')
+const noteRouter=require('./routes/noteRouter')
+
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+
+//Routes
+app.use('/users', userRouter)
+app.use('/api/notes', noteRouter)
+
+//Connect to mongoDB
+const URI = process.env.MONGODB_URL
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB Connected : ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit()
+    }
+}
+connectDB()
+//Listening on port
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log('Server is running on port : ', PORT)
+})
+
