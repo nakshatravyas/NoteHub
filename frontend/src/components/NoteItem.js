79f43a45457198125a49d1b33458c918ac0c03af
Nakshatra Vyas <125440526+nakshatravyas@users.noreply.github.com>
2024-02-06 18:13:12 +0530

    Update NoteItem.js

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -8,8 +8,8 @@ export default function NoteItem({ note }) {
     return (
         
         <Link to={`editnote/${note._id}`} className='note'>
-            <h4>{note.title.length > 40 ? (note.title.substr(0, 8)) + '...' : note.title}</h4>
-            <h4>{note.content.length > 40 ? (note.content.substr(0, 8)) + '...' : note.content}</h4>
+            <h4>{note.title.length > 10 ? (note.title.substr(0, 8)) + '...' : note.title}</h4>
+            <h4>{note.content.length > 8 ? (note.content.substr(0, 8)) + '...' : note.content}</h4>
             <p><TimeAgo date={note.date} formatter={formatter} /></p>
         </Link>
         
